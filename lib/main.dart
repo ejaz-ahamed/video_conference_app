b933eadad19ea71244c531ed0b4b7a045232584a
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-03-21 11:27:21 +0530

    Added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:video_conference_app/firebase_options.dart';
+import 'package:video_conference_app/utils/colors.dart';
 import 'package:video_conference_app/view/home_page.dart';
 
 void main() async {
@@ -19,10 +20,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Video Conference App',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
       ),
       home: const HomePage(),
     );
